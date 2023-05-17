661ffa04dae7dedf3e7d4ba36f4d27223e5e9bbc
ueunli <cdenen20@gmail.com>
2023-05-17 15:50:06 +0900

    Config: 프로젝트 초기환경 세팅
    - .gitignore, 프로젝트 생성
    - 스토리보드 삭제
    
    Closes #1

---
+++ Booky/Booky/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Booky
+//
+//  Created by J.E on 2023/05/17.
+//
+
+import UIKit
+
+final class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ios-book-app
+# 부키<sup> (Booky)</sup>
+> 심플한 애독가를 위한 'Book-Key', Booky 앱
+
