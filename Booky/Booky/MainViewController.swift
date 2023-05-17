b0a4a124bf81ec30bd43e9fadd183c39b5cabe71
ueunli <cdenen20@gmail.com>
2023-05-17 15:31:34 +0900

    Config #1: 스토리보드 제거
    - `main.storyboard` 및 유관 빌드설정 제거
    - 네이밍 및 변경) ViewController → MainViewController
    - SceneDelegate의 scene(_:willConnectTo:options:) 메서드 로직 추가
    - Deployment 조건 세팅

--- Booky/Booky/MainViewController.swift
+++ Booky/Booky/MainViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
