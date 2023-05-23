d44b30ab578fe1f6d4d5096a61d8b50cd912bcf5
ueunli <cdenen20@gmail.com>
2023-05-23 15:42:03 +0900

    Feat #5: RouterType 구현
    - RouterType: 요청 상황별 endpoint를 구성하기 위해 거쳐가는 타입으로서의 요구사항과 기본메서드 구현
    - Alamofire, Kingfisher의 메서드로 request를 생성·받아오는 로직을 기본메서드로 정의

---
+++ Booky/Booky/Network/Router/Interface/RouterType.swift
@@ -0,0 +1,67 @@
+//
+//  RouterType.swift
+//  Booky
+//
+//  Created by J.E on 2023/05/23.
+//
+
+import Alamofire
+import Kingfisher
+import Foundation
+
+protocol RouterType {
+    
+    var scheme: String { get }
+    var host: String { get }
+    var staticPath: [String] { get }
+    var optionPath: [String] { get set }
+    var staticQuery: [String: Any] { get }
+    var optionQuery: [String: Any] { get set }
+    
+}
+
+extension RouterType {
+    
+    static var defaultComponents: URLComponents {
+        URLComponents()
+    }
+    
+    var urlComponents: URLComponents {
+        var components = Self.defaultComponents
+        components.scheme = scheme
+        components.host = host
+        components.path = (staticPath + optionPath).joined(separator: "/")
+        components.queryItems = staticQuery.map { URLQueryItem(name: $0.0, value: "\($0.1)") }
+        components.queryItems?.append(contentsOf: optionQuery.map { URLQueryItem(name: $0.0, value: "\($0.1)") })
+        return components
+    }
+    
+    mutating func updatePath(_ newPaths: String...) {
+        optionPath.append(contentsOf: newPaths)
+    }
+    
+    mutating func updateQuery(_ newQueries: [String: Any]) {
+        newQueries.forEach {
+            optionQuery.updateValue($0.0, forKey: "\($0.1)")
+        }
+    }
+    
+}
+
+extension RouterType {
+    
+    func fetchBookList(_ completion: @escaping (BookListDTO?) -> Void) {
+        let request = AF.request(urlComponents)
+        request.responseDecodable(of: BookListDTO.self) { response in
+            let data = response.value
+            completion(data)
+        }
+    }
+    
+    func fetchBookCover(from link: String, _ completion: @escaping (KFImage) -> Void) {
+        let url = URL(string: link)
+        let kfImage = KFImage(url)
+        completion(kfImage)
+    }
+    
+}
