2c1642b969730f3045122099f03c3af31616f994
ueunli <cdenen20@gmail.com>
2023-05-25 00:23:21 +0900

    Feat #6: 도서 카테고리별 번호와 명칭을 담은 BookCategory 열거형 정의
    - CaseIterable 채택
    - 추후 세부카테고리 및 외국도서 case 추가될 수 있음

---
+++ Booky/Booky/Models/BookCategory.swift
@@ -0,0 +1,112 @@
+//
+//  BookCategory.swift
+//  Booky
+//
+//  Created by J.E on 2023/05/24.
+//
+
+enum BookCategory: Int, CaseIterable {
+    
+    case literature = 1
+    case history = 74
+    case economicsManagement = 170
+    case selfDevelopment = 336
+    case computerMobile = 351
+    case artsAndPopCulture = 517
+    case humanities = 656
+    case socialSciences = 798
+    case science = 987
+    case children = 1108
+    case youth = 1137
+    case travel = 1196
+    case cookingAndLiving = 1230
+    case religionAndOccult = 1237
+    case foreignLanguage = 1322
+    case examAndCertificationGuideBooks = 1383
+    case goodParents = 2030
+    case comics = 2551
+    case magazines = 2913
+    case koreanBookWrittenInEnglish = 8981
+    case audioBooks = 6653
+    case universityTextbooks = 8257
+    case toddlers = 13789
+    case completeWorks = 17195
+    case governmentEmployeeExamGuideBooks = 34582
+    case elementaryWorkBooks = 50246
+    case essays = 55889
+    case healthAndHobby = 55890
+    case middleSchoolWorkBooks = 76000
+    case highSchoolWorkBooks = 76001
+    case genreNovels = 112011
+    case biggerFontBooks = 184996
+    
+    var name: String {
+        switch self {
+        case .literature:
+            return "소설/시/희곡"
+        case .history:
+            return "역사"
+        case .economicsManagement:
+            return "경제경영"
+        case .selfDevelopment:
+            return "자기계발"
+        case .computerMobile:
+            return "컴퓨터/모바일"
+        case .artsAndPopCulture:
+            return "예술/대중문화"
+        case .humanities:
+            return "인문학"
+        case .socialSciences:
+            return "사회과학"
+        case .science:
+            return "과학"
+        case .children:
+            return "어린이"
+        case .youth:
+            return "청소년"
+        case .travel:
+            return "여행"
+        case .cookingAndLiving:
+            return "요리/살림"
+        case .religionAndOccult:
+            return "종교/역학"
+        case .foreignLanguage:
+            return "외국어"
+        case .examAndCertificationGuideBooks:
+            return "수험서/자격증"
+        case .goodParents:
+            return "좋은부모"
+        case .comics:
+            return "만화"
+        case .magazines:
+            return "잡지"
+        case .koreanBookWrittenInEnglish:
+            return "영어로 쓰인 한국서적"
+        case .audioBooks:
+            return "오디오북"
+        case .universityTextbooks:
+            return "대학교재"
+        case .toddlers:
+            return "유아"
+        case .completeWorks:
+            return "전집"
+        case .governmentEmployeeExamGuideBooks:
+            return "공무원 수험서"
+        case .elementaryWorkBooks:
+            return "초등참고서"
+        case .essays:
+            return "에세이"
+        case .healthAndHobby:
+            return "건강/취미"
+        case .middleSchoolWorkBooks:
+            return "중학교참고서"
+        case .highSchoolWorkBooks:
+            return "고등학교참고서"
+        case .genreNovels:
+            return "장르소설"
+        case .biggerFontBooks:
+            return "큰글자책"
+        }
+    }
+    
+}
