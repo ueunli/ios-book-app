8ce4c46c7fac51821f803a34a4a7dc03a9ebab92
ueunli <cdenen20@gmail.com>
2023-05-24 16:05:27 +0900

    Feat #6: 각 도서에 유저가 직접 추가할 수 있는 데이터 타입 구현
    - Note 구조체, UserBookDetail 구조체 정의
    - 좋아요, 진행도, 메모를 저장 및 UserDefaults용 메서드를 가짐
    - 도서 리스트 뷰에서도 표시할 수 있도록 해당 도서와 매칭하기 위해 isbn을 가짐

---
+++ Booky/Booky/Models/Note.swift
@@ -0,0 +1,24 @@
+//
+//  Note.swift
+//  Booky
+//
+//  Created by J.E on 2023/05/24.
+//
+
+import Foundation
+
+struct Note {
+    
+    var type: NoteType
+    var title: String
+    var body: String
+    
+    init(type: NoteType,
+         title: String = Date(timeIntervalSinceNow: 0).description,
+         body: String = "") {
+        self.type = type
+        self.title = title
+        self.body = body
+    }
+    
+}
