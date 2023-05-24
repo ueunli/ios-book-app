4d503d22f3e3668d619c76c3e6fa3b1d54c1972d
ueunli <cdenen20@gmail.com>
2023-05-24 22:51:26 +0900

    Design #7: UIImage 확장
    - 뷰에 사용될 아이콘 이미지를 UIImage? 타입상수로 선언
    - 터치에 반응하여 색상이 변경될 여지가 있는 이미지는 SFSymbol로, 색상이 고정적인 이미지는 Asset을 활용
    - Refactor: 기존 MainViewController에서 탭 바 아이콘 인자로 UIImage를 직접 생성할 필요 없이 위에서 추가한 상수를 곧장 불러오도록 변경

---
+++ Booky/Booky/Extensions/UIImage+.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+.swift
+//  Booky
+//
+//  Created by J.E on 2023/05/24.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    static let searchIcon = UIImage(named: "search")
+    static let starsIcon = UIImage(named: "stars")
+    static let homeIcon = UIImage(systemName: "house")
+    static let starIcon = UIImage(systemName: "star")
+    
+}
